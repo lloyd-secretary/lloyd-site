881797304d83bb2a6279d3663d3bb2583f07db2a
kgauld1 <kgauld1@gmail.com>
2022-09-03 21:34:39 -0400

    remove top5

--- app/rotation/forms.py
+++ app/rotation/forms.py
@@ -1,11 +1,7 @@
 from flask_wtf import Form
-from wtforms import IntegerField, SelectMultipleField
+from wtforms import IntegerField
 from wtforms.validators import DataRequired
 
 class CommentUpdateForm(Form):
 	comment = IntegerField('comment', validators=[DataRequired()])
 	rating = IntegerField('rating', validators=[DataRequired()])
-
-
-#class Top5(Form):
-#	tops = SelectMultipleField('comment', choices=[], validators=[DataRequired()])
