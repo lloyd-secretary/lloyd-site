b017df9a553bafbc08a4542397c4b22fed7e7ae8
Pranav Patil <4pranavpatil@gmail.com>
2023-03-25 18:33:03 -0700

    Only set up SSL for local mode, on server apache2 handles SSL

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
 import os
 from app import app, production
 
-# this will really only matter
-if not production:
-    from OpenSSL import SSL
-    context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
-    context.use_privatekey_file('/etc/ssl/lloyd.caltech.edu.key')
-    context.use_certificate_file('/etc/ssl/lloyd.caltech.edu.crt')
-
 if __name__== '__main__':
-    app.run(debug=(not production), threaded=(not production), ssl_context='adhoc' if (not production) else context)
+    app.run(debug=(not production), threaded=(not production), ssl_context='adhoc' if (not production) else None)
