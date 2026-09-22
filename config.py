b1ce5b929ea42534b2dc90f95e98069e5b875403
lloyd-secretary <61448655+lloyd-secretary@users.noreply.github.com>
2026-09-21 17:38:22 -0700

    Update database bind from rotation2026 to rotation2025

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 SQLALCHEMY_DATABASE_URI = 'mysql://web@localhost/lloyd?charset=utf8mb4'
 SQLALCHEMY_BINDS = {
-    'rotation2026': 'mysql://web@localhost/rotation2026?charset=utf8mb4',
+    'rotation2025': 'mysql://web@localhost/rotation2025?charset=utf8mb4',
 }
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
